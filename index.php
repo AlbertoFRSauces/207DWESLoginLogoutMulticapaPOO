31751edd68cc400013cb26122858bae56c5e12ec
Alberto <alberto.ferram@educa.jcyl.es>
2021-12-21 18:34:05 +0100

    21/12/2021 Creación de la estructura del Proyecto LoginLogoutMulticapaPOO. Configuración del gitignore Configuración del archivo configDBPDO, configAPP Configuración de README.md Configuración de scripts(Crea, Carga, Borra) tanto para explotacion como para desarrollo. Configuración .htaccess Configuración de archivos de errores(400, 403, 404, 500) para el .htaccess Add de index. Add de layout.php, vIncio.php, vLogin.php en la view Add de cInicio.php, cLogin.php en el controller Add de UsuarioPDO.php, Usuario.php, BBPDO.php en el model

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @author: Alberto Fernandez Ramirez
+ * @since: 25/11/2021
+ * @version: 1.0 Realizacion del index
+ * @copyright: Copyright (c) 2021, Alberto Fernandez Ramirez
+ * Index para entrar al login o salir hacia el index del Tema 5
+ */
+
+
+
+?>
